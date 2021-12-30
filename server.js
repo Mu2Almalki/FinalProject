bfe56195b58bbc8e8435e157739610212c03ecd1
Mu2Almalki <Mushira.Almalki.ruh.js@tuwaiq.edu.sa>
2021-12-30 14:02:29 +0300

    test

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/payment',routerStripe)
 //     console.log("express has started!")
 // })
 
-app.use("/", express.static(path.join(__dirname, "../frontend/build")));
+app.use("/", express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) => {
   console.log(path.resolve(__dirname, "frontend/build/index.html"));
   res.sendFile(path.resolve(__dirname + "/frontend/build", "index.html"));
